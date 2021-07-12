f317d8770b62e09f925ea8e5a1acc4f406d83f4d
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-07-12 15:35:13 -0400

    update build

--- source/elements/color.js
+++ source/elements/color.js
@@ -1,5 +1,9 @@
+import color from '../templates/color.js';
+
 export class Color extends Quantum {
     constructor() {
         super();
     }
 }
+
+Color.define('quantum-color', color);
--- source/main.js
+++
@@ -1,4 +0,0 @@
-import { Color } from './elements/color.js';
-import color from './templates/color.js';
-
-Color.define('quantum-color', color);
